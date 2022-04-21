1121b9d91ef05dc8d838b1d4f8651404515b251e
idavoong <60492208+idavoong@users.noreply.github.com>
2022-04-20 17:51:59 -0700

    some changes to main button and timer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./styles.css";
-import React, { useState } from "react"
-
+import React, { useState } from "react";
 
 export default function App() {
+  // x and y for main button
+  const [placeX, setplaceX] = useState(500);
+  const [placeY, setplaceY] = useState(500);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
@@ -18,23 +21,35 @@ export default function App() {
         {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Red_Color.jpg/1536px-Red_Color.jpg" class="red image" />
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png" class="green image" /> */}
 
-        {/* <button class="green image"></button> */}
+        {/* main button that user clicks on */}
+        <button
+          class="green image area"
+          style={{ top: placeY, left: placeX }}
+          onClick={() => setplaceX(Math.floor(Math.random() * 300) + 1)}
+          onClick={() => setplaceY(Math.floor(Math.random() * 500) + 1)}
+        ></button>
       </div>
     </div>
   );
 }
 
+//function for making timer
 function Timer(props) {
-  const [timer, setTimer] = useState(120);
-  function countdown(){
-    setTimer(timer-1);
+  const [timer, setTimer] = useState(60);
+  function countdown() {
+    console.log(timer);
+    setTimer(timer - 1);
+    console.log(timer, "timer 2");
   }
-  // let secore = 0;
+  // let score = 0;
   return (
     <div>
       {timer}
-    <button class="green image" onClick={() => setInterval(countdown, 1000)}>Click to Start</button>
+      <button class="green image" onClick={() => setInterval(countdown, 1000)}>
+        Click to Start
+      </button>
     </div>
+
     // {props.place} - {population} residents{" "}
 
     // <div>
@@ -46,13 +61,30 @@ function Timer(props) {
   );
 }
 
-//edit later, use to keep track of scoreboard
+/* 
+- the score is updating but only 
+when the button is clicked
+
+- need to remove button onClick and replace 
+it with the button/image that it is supposed 
+to be clicked on -> so the button in the blue box*/
+
 function Score(props) {
   const [score, setScore] = useState(0);
-  // let secore = 0;
+  // let score = 0;
+  function addScore() {
+    setScore(score + 1);
+  }
   return (
-    //have to make sure button cannot be clicked on
-    <button class="green image" onClick={() => setScore(score + 1)}>Button</button>
+    <div>
+      {score}
+
+      <button class="green image" onClick={() => setScore(addScore, 0)}>
+        Click to Start
+      </button>
+
+      {/* <button class="green image" onClick={() => setScore(score + 1)}>Button</button> */}
+    </div>
     // <div>
     //   {props.place} - {population} residents{" "}
     //   <button onClick={() => setPopulation(population + 1)}>+</button>
@@ -61,4 +93,3 @@ function Score(props) {
     //{/* NYC - {population} residents <button onClick={() => null}>+</button> */}
   );
 }
-
