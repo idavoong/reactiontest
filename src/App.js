e1a94b87cf16fe7fa18bd658f2b5d5ef63de1b0d
idavoong <60492208+idavoong@users.noreply.github.com>
2022-05-04 18:00:01 -0700

    fixed main button

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React, { useState, useEffect } from "react";
 
 export default function App() {
   // x and y for main button
-  const [placeX, setplaceX] = useState(500);
-  const [placeY, setplaceY] = useState(500);
+  const [placeX, setplaceX] = useState(700);
+  const [placeY, setplaceY] = useState(200);
   // set score to 0
   const [score, setScore] = useState(0);
 
   //function for xy movement
   function buttonRandom() {
-    setplaceX(Math.floor(Math.random() * 300) + 1);
-    setplaceY(Math.floor(Math.random() * 500) + 1);
+    setplaceX(Math.floor(Math.random() * 1400) + 1);
+    setplaceY(Math.floor(Math.random() * 300) + 1);
     setScore(addScore, 0);
   }
 
@@ -25,6 +25,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
 
+      {/* box for timer and scoreboard */}
       <div class="tracker">
         <h1>Timer:</h1>
         <Timer />
